029a11d8db3c544fa3b2a6e8b0ee42210d7837f8
callumreillyy <callumreilly2911@outlook.com>
2024-05-02 09:26:18 +0000

    added routes and controller for index and surveys

---
+++ app/Controllers/EvalController.php
@@ -0,0 +1,24 @@
+  <?php namespace App\Controllers;
+
+ use CodeIgniter\Controller;
+
+ class EvalController extends BaseController
+ {
+     public function __construct()
+     {
+         // Load the URL helper, it will be useful in the next steps
+         // Adding this within the __construct() function will make it 
+         // available to all views in the ResumeController
+         helper('url'); 
+     }
+
+     public function index()
+     {
+         return view('index');
+     }
+
+     public function surveyss()
+     {
+         return view('surveys');
+     }
+ }
